b6995deb43999e78796a4375acbeae7b5df87e59
WhiteBunnyI <staskuznetsov87@gmail.com>
2025-03-31 22:49:04 +0200

    Making frontend...

--- src/app/web/script.js
+++ src/app/web/script.js
@@ -2,6 +2,7 @@ let isInit = false;
 let isMobile = false;
 let currency_value = null;
 let arrows = null;
+let switchCurrency = null;
 
 document.addEventListener("DOMContentLoaded", initialization);
 window.addEventListener("resize", check_resolution);
@@ -10,6 +11,7 @@ function initialization()
 {
     arrows = document.querySelector("div.flex-container.currency-value img.arrows");
     currency_value = document.querySelector("div.flex-container.currency-value");
+    switchCurrency = document.querySelector("div.flex-container.currency img");
     isInit = true;
     check_resolution();
 }
@@ -32,18 +34,26 @@ function check_resolution()
 
 function setMobile()
 {
-    
     isMobile = true;
+    
     currency_value.style.flexFlow = "column";
+    currency_value.parentElement.style.flexFlow = "row";
+
     arrows.src = "./icons/sync_alt.svg";
     arrows.className = "mobile-arrows";
+
+    switchCurrency.parentElement.parentElement.style.flexFlow = "column";
 }
 
 function setDesktop()
 {
     isMobile = false;
+
     currency_value.style.flexFlow = "row";
+    currency_value.parentElement.style.flexFlow = "column";
+
     arrows.src = "./icons/arrows.svg";
     arrows.className = "arrows";
 
+    switchCurrency.parentElement.parentElement.style.flexFlow = "row";
 }
