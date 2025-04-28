84213d1f4f4f9cee92bb1c75db389bd2c5a6ccd1
WhiteBunnyI <144366124+WhiteBunnyI@users.noreply.github.com>
2025-04-28 19:51:20 +0200

    Making unit test...

--- src/tests/test_database.py
+++ src/tests/test_database.py
@@ -1,5 +1,4 @@
 import sqlite3
-import unittest.mock
 import pytest
 import os
 from pytest_mock import MockerFixture
@@ -7,6 +6,11 @@ try:
     import src.app.app as server    #Нужна для того, чтобы мой IDE выдавал подсказки
 except ImportError:
         import app as server
+
+
+@pytest.fixture
+def get_rows():
+    return ['currency', 'rate']
 
 
 @pytest.fixture
@@ -22,45 +26,60 @@ def get_db():
         db.close()
     
 
+@pytest.fixture
+def init_db(get_db: sqlite3.Connection):
+    server.init_db()
+    return get_db
+
+
+@pytest.fixture
+def get_currency():
+    return [{'currency': 'USD', 'rate': 1.1357}, {'currency': 'JPY', 'rate': 162.8}, {'currency': 'BGN', 'rate': 1.9558}, {'currency': 'EUR', 'rate': 1.0}]
+
+
+@pytest.fixture
+def import_db(mocker: MockerFixture, init_db: sqlite3.Connection, get_currency):
+    currency = ""
+    for i in get_currency:
+        currency.join(f"<Cube currency='{i["currency"]}' rate='{i["rate"]}' />\n")
+        
+    mock_get = mocker.patch("requests.get")
+    mock_get.return_value.content = f'''<?xml version="1.0" encoding="UTF-8"?>
+    <gesmes:Envelope xmlns:gesmes="http://www.gesmes.org/xml/2002-08-01" xmlns="http://www.ecb.int/vocabulary/2002-08-01/eurofxref">
+        <gesmes:subject>Reference rates</gesmes:subject>
+        <gesmes:Sender>
+            <gesmes:name>European Central Bank</gesmes:name>
+        </gesmes:Sender>
+        <Cube>
+            <Cube time='2025-04-25'>
+                {currency}
+            </Cube>
+        </Cube>
+    </gesmes:Envelope>
+        '''.encode()
+    with pytest.raises(SystemExit):
+        server.import_rates()
+    return init_db
+
+
 def test_get_db(get_db: sqlite3.Connection):
     assert get_db != None
 
-@pytest.fixture
-def get_rows():
-    return ['currency', 'rate']
 
-def test_init_db(get_db : sqlite3.Connection, get_rows):
-    server.init_db()
-    cursor = get_db.cursor()
+def test_init_db(init_db: sqlite3.Connection, get_rows):
+    cursor = init_db.cursor()
     cursor.execute('SELECT * FROM currency_rates')
     rows = [des[0] for des in cursor.description]
     assert rows == get_rows
-    
-def test_get_rates(get_db : sqlite3.Connection, get_rows):
-    server.init_db()
+
+
+def test_import_rates(import_db: sqlite3.Connection, get_currency):
+    cursor = import_db.cursor()
+    cursor.execute('SELECT * FROM currency_rates')
+    rows = [dict(row) for row in cursor.fetchall()]
+    assert rows == get_currency
+
+
+def test_get_rates(import_db: sqlite3.Connection, get_currency):
     rows = server.get_rates()
-    print(dir(rows))
-    assert rows == get_rows
-    
-def test_import_rates(mocker: MockerFixture, get_db: sqlite3.Connection):
-    mock_get = mocker.patch("requests.get")
-    mock_get.return_value.content = '''<?xml version="1.0" encoding="UTF-8"?>
-<gesmes:Envelope xmlns:gesmes="http://www.gesmes.org/xml/2002-08-01" xmlns="http://www.ecb.int/vocabulary/2002-08-01/eurofxref">
-	<gesmes:subject>Reference rates</gesmes:subject>
-	<gesmes:Sender>
-		<gesmes:name>European Central Bank</gesmes:name>
-	</gesmes:Sender>
-	<Cube>
-		<Cube time='2025-04-25'>
-			<Cube currency='USD' rate='1.1357'/>
-			<Cube currency='JPY' rate='162.80'/>
-			<Cube currency='BGN' rate='1.9558'/>
-		</Cube>
-	</Cube>
-</gesmes:Envelope>
-    '''.encode()
-    
-    server.init_db()
-    with pytest.raises(SystemExit):
-        server.import_rates()
-    assert True
+    assert rows.get_json() == get_currency
